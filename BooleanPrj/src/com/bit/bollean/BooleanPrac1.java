604a562c0dc974aea231c2c86c4d8538c63ddd75
Foysal <foysal_1977@yahoo.com>
2014-05-15 12:54:15 -0400

    gg

---
+++ BooleanPrj/src/com/bit/bollean/BooleanPrac1.java
@@ -0,0 +1,29 @@
+package com.bit.bollean;
+
+public class BooleanPrac1
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println(yr());
+		
+
+	}
+	
+	static boolean yr()
+	{
+		if(4<3)
+		{
+			return true;
+			
+		}
+		else 
+		{
+			return false;
+			
+		}
+		
+	}
+
+}
