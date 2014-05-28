ff227edd8577c0c49f1d52c9128fcf8c9430ca21
asadShalok <asadfrombronx@yahoo.com>
2014-05-28 13:16:15 -0400

    add file

--- GitPractice040514/src/com/bit/test/BaseTest.java
+++ GitPractice040514/src/com/bit/test/BaseTest.java
@@ -1,18 +1,27 @@
 package com.bit.test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest
 {
 	
 	static FirefoxDriver dr =new FirefoxDriver();
-	public static void main(String[] args)
+	public static void main(String[] args) throws Throwable
 	{
 		start();
 	}
-	public static void start()
+	public static void start() throws Exception 
 	{
 		dr.get("http://www.bluefly.com");
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println(dr.getTitle());
+		boolean b=dr.getPageSource().contains("bluefly");
+		System.out.println(b);
+		System.out.println(dr.getTitle().contains("bluefly"));
+		Thread.sleep(3000);
 		dr.quit();
 	}
 	
