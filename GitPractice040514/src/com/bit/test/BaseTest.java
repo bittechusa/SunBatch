9c80a260f0d53d60b20a96362e54aa84f39deb20
unknown <touhidhossain@ymail.com>
2014-06-15 16:50:44 -0400

    hi

--- GitPractice040514/src/com/bit/test/BaseTest.java
+++ GitPractice040514/src/com/bit/test/BaseTest.java
@@ -1,20 +1,29 @@
 package com.bit.test;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest
 {
 	
 	static FirefoxDriver dr =new FirefoxDriver();
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		start();
 	}
-	public static void start()
+	public static void start() throws Exception
 	{
 		dr.get("http://www.bluefly.com");
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println(dr.getTitle());
+		boolean b=dr.getPageSource().contains("bluefly");
+		System.out.println(b);
+		System.out.println(dr.getTitle().contains("Bluefly"));
+		Thread.sleep(3000);
 		dr.quit();
 	}
 	
-
 }
